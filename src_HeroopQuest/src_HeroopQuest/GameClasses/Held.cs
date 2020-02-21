d78ad29559f53f11c97a44a3b5e2be4c7489916b
Alfian Sanjaya <alfian.sanjaya2000@gmail.com>
2020-02-21 11:54:47 +0100

    Constructor HeldKlasse

--- src_HeroopQuest/src_HeroopQuest/GameClasses/Held.cs
+++ src_HeroopQuest/src_HeroopQuest/GameClasses/Held.cs
@@ -7,6 +7,37 @@ namespace src_HeroopQuest.GameClasses
     enum HeldType { Barbaar, Dwerg, Elf, Tovenaar }
     class Held
     {
+        public Held(HeldType type, string naam)
+        {
+            Naam = naam;
+            HeldType = type;
+            switch (type)
+            {
+                case HeldType.Barbaar:
+                    AanvalDobbelsteen = 3;
+                    MaxIntelligentie = 2;
+                    MaxLichaam = 8;
+                    break;
+                case HeldType.Dwerg:
+                    MaxIntelligentie = 3;
+                    MaxLichaam = 7;
+                    break;
+                case HeldType.Elf:
+                    MaxIntelligentie = 4;
+                    MaxLichaam = 6;
+                    break;
+                case HeldType.Tovenaar:
+                    MaxIntelligentie = 6;
+                    MaxLichaam = 4;
+                    AanvalDobbelsteen = 1;
+                    break;
+                default:
+                    break;
+            }
+            HuidigIntelligentie = MaxIntelligentie;
+            HuidigLichaam = MaxLichaam;
+        }
+
         public string Naam { get; set; }
         public string Beschrijving { get; private set; }
         public int MaxIntelligentie { get; private set; }
